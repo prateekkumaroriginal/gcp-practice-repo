c93c5a1fb418e47b4ae5d97d1cc1d0062f289010
prateekkumaroriginal <prateek.kumar.original@gmail.com>
2026-05-04 16:32:55 +0530

    fix

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { prisma } from "@gcp-turborepo/prisma";
+import { prisma } from "@gcp-turborepo/prisma/client";
 
 const app = express();
 app.use(express.json());
