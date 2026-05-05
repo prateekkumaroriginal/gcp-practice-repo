ba47d13f01ed2869da98409b9efe173fd7a09d63
prateekkumaroriginal <prateek.kumar.original@gmail.com>
2026-05-05 13:29:29 +0530

    fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@ import { prisma } from "@gcp-turborepo/prisma/client";
 
 export default async function Home() {
   console.log("DB URL:", process.env.DATABASE_URL);
-  const user = await prisma.user.findFirst();
+  // const user = await prisma.user.findFirst();
   return (
     <div>
       {/* <p>{user?.email}</p>
