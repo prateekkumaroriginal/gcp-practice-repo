64793a06b9296f01a7155facb5c39603cda37cb6
prateekkumaroriginal <prateek.kumar.original@gmail.com>
2026-05-05 11:59:07 +0530

    add db interaction

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,11 @@
 import { prisma } from "@gcp-turborepo/prisma/client";
 
 export default async function Home() {
-  // const user = await prisma.user.findFirst();
+  const user = await prisma.user.findFirst();
   return (
     <div>
-      {/* <p>{user?.email}</p>
-      <p>{user?.password}</p> */}
+      <p>{user?.email}</p>
+      <p>{user?.password}</p>
     </div>
   )
 }
